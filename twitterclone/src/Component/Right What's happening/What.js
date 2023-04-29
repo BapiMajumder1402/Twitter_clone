7c07df92be19728b23ff0219aae4665da8cdb9ac
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2023-04-29 18:39:01 +0530

    right component added

---
+++ twitterclone/src/Component/Right What's happening/What.js
@@ -0,0 +1,20 @@
+import React from "react"
+export const What = [
+    {
+        tag: "# Functionup's student are making Twitter",
+        tweet: "22.5k"
+    },
+    {
+        tag: "# Elon Musk whats to know location",
+        tweet: "45.8k"
+    },
+    {
+        tag: "# He wants to sell Twitter",
+        tweet: "58.9k"
+    },
+    {
+        tag: "# A rockect just launched from spaceX",
+        tweet: "69.5k"
+    },
+]
+

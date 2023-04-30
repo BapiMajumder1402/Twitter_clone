d364c5c55b7994e37f3c731b1b623f4e70729dce
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2023-05-01 03:34:30 +0530

    Like functionality and follow un follow button added

--- src/Component/FeedComponent/Feed.js
+++ src/Component/FeedComponent/Feed.js
@@ -1,32 +1,43 @@
+import { useState } from 'react';
 import f from "./Feed.module.css"
 import { AiOutlineHeart } from 'react-icons/ai'
 import { RxLoop } from 'react-icons/rx'
 import { FaRegComment } from 'react-icons/fa'
 
 function Feed({ data }) {
+    const [likeCount, setLikeCount] = useState(data.likeCount);
+    const [liked, setLiked] = useState(false);
+    const handleLikeClick = () => {
+        if (liked) {
+            setLikeCount(likeCount - 1);
+        } else {
+            setLikeCount(likeCount + 1);
+        }
+        setLiked(!liked);
+    };
     return (
-        <div className={f.parent} >
-            <div className={f.Header} >
-                <div className={f.Dp} >
+        <div className={f.parent}>
+            <div className={f.Header}>
+                <div className={f.Dp}>
                     <img src={data.tweetedBy.profilePhoto} className={f.DpImg} />
                 </div>
                 <div className={f.NameContainer}>
-                    <p className={f.name} >{data.tweetedBy.name}</p>
-                    <p className={f.content} >{data.content}</p>
+                    <p className={f.name}>{data.tweetedBy.name}</p>
+                    <p className={f.content}>{data.content}</p>
                     <div className={f.Body}>
                         <img src={data.image} className={f.bodyImage} />
-                        <div className={f.Footer} >
-                            <div><AiOutlineHeart /> {data.likeCount}</div>
+                        <div className={f.Footer}>
+                            <div onClick={handleLikeClick}>
+                                <AiOutlineHeart style={{ color: liked ? 'red' : 'grey' }} /> {likeCount}
+                            </div>
                             <div><FaRegComment /> {data.commentCount}</div>
                             <div><RxLoop /> {data.reTweetsCount}</div>
                         </div>
-
                     </div>
-                </div >
-
-
+                </div>
             </div>
-
-        </div>)
+        </div>
+    );
 }
+
 export default Feed;

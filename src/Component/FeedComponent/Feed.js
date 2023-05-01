7ffa4c83792fdf3b5adaa38595cac85f9b37b7f4
Akhilesh <akhileshsharma2429@gmail.com>
2023-05-01 09:54:26 +0530

    done

--- src/Component/FeedComponent/Feed.js
+++ src/Component/FeedComponent/Feed.js
@@ -1,35 +1,11 @@
-import { useState } from 'react';
 import f from "./Feed.module.css"
 import { AiOutlineHeart } from 'react-icons/ai'
 import { RxLoop } from 'react-icons/rx'
 import { FaRegComment } from 'react-icons/fa'
-import { useState } from 'react'
 
 
 function Feed({ data }) {
-    const [likeCount, setLikeCount] = useState(data.likeCount);
-    const [liked, setLiked] = useState(false);
-    const handleLikeClick = () => {
-        if (liked) {
-            setLikeCount(likeCount - 1);
-        } else {
-            setLikeCount(likeCount + 1);
-        }
-        setLiked(!liked);
-    };
-    // const [like, setLike] = useState(false)
-    // const [likecount, setLikeCount] = useState(data.likeCount)
 
-    // function handleLike() {
-
-    //     if (like===false) {
-    //         setLikeCount(likecount+1)
-    //     }
-    //     else {
-    //         setLikeCount(likecount-1)
-    //     }
-
-    // setLike(true)}
     return (
         <div className={f.parent}>
             <div className={f.Header}>
